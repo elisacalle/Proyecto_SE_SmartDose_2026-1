cf11d003d8fefbd35be6b141f59919ba1ad2ff3b
davidaristigir-cmyk <david.aristigir@gmail.com>
2026-05-24 21:33:21 -0500

    Update main.c

--- Firmware/src/main/main.c
+++ Firmware/src/main/main.c
@@ -1,29 +1,3 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <stdbool.h>
-#include <stdint.h>
-#include <stdarg.h>
- 
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/semphr.h"
- 
-#include "driver/gpio.h"
-#include "driver/ledc.h"
-#include "driver/i2c.h"
-#include "driver/uart.h"
- 
-#include "nvs_flash.h"
-#include "nvs.h"
-#include "esp_log.h"
-#include "esp_timer.h"
- 
-// -----------------------------------------------------------------------------
-// PINES
-// -----------------------------------------------------------------------------
- 
 #define SERVO_GPIO_T1   14
 #define SERVO_GPIO_T2   27
 #define SERVO_GPIO_T3   16
